6e3edf180a755a44a568246ba89962c4eb51e5d6
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-07-18 02:19:29 +0300

    created model Userprfile

--- AwardsApp/models.py
+++ AwardsApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'profile/')
+    bio = models.TextField()
+    phone_number = models.IntegerField()
+
+    
--- AwardsProject/settings.py
+++ AwardsProject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'AwardsProject.urls'
