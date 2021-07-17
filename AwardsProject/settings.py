f523209fa0ce0e2b9f8b67ae0fee6b71eedfb32b
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-07-18 01:31:11 +0300

    created an authentication system

--- AwardsProject/settings.py
+++ AwardsProject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
