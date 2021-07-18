54a9df042d3dee828ca2a030685ec9f9c2fa13ce
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-07-18 13:36:11 +0300

    added view function to display user profile

--- AwardsApp/urls.py
+++ AwardsApp/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    path('profile/', views.profile_view, name='profile')
     # url(r'^api/movie/$', views.MovieList.as_view()),
     # url(r'^api/book/$', views.BookList.as_view())
 ]
--- AwardsApp/views.py
+++ AwardsApp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 # @login_required(login_url='/accounts/login/')
 def index(request):
     return render(request, 'awards/index.html')
+
+def profile_view(request):
+    user = request.user
+    user = User.objects.get(username = user.username)
+
+    return render (request, 'awards/profile.html', {"user":user})
