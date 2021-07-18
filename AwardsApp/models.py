7250393dc04d6b06ed117f7f4f3a270e9aaf7963
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-07-18 12:59:03 +0300

    methods for savind and deleteing a profile

--- AwardsApp/models.py
+++ AwardsApp/models.py
@@ -1,14 +1,25 @@
 from django.db import models
 from django.db.models.aggregates import Max
 from django.db.models.base import Model
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_photo = models.ImageField(upload_to = 'profile/')
     bio = models.TextField()
     phone_number = models.IntegerField()
+
+    def __str__(self):
+        return self.user
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+        
 
 class Project(models.Model):
     project_title = models.CharField(max_length= 30)
@@ -16,6 +27,9 @@ class Project(models.Model):
     project_description = models.TextField()
     project_link = models.URLField()
     profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.project_title
 
 class Rating(models.Model):
     design = models.IntegerField(choices=list(zip(range(1, 11), range(1, 11))), default=0)
